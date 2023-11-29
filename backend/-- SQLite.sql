8f16342b728ad0d92f88992ad924cfd9dce416c2
Milie971 <e.lolia971@gmail.com>
2023-11-29 16:17:52 +0100

    il faut faire toutes les commandes sql fonctonner

---
+++ backend/-- SQLite.sql
@@ -0,0 +1,83 @@
+-- SQLite
+INSERT INTO ad (title, owner);
+ VALUES
+('Blouson', 'robert@dupont.fr'),
+('Short', 'regis@durand.fr'),
+('Chaussette', 'isabelle@jaures.fr');
+
+-- SQLite
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Switch', 'Switch red and blue….', 'Emilie', 150, ' https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.phonandroid.com%2Fnintendo-switch-mario-rouge-bleu-acheter-meilleur-prix.html&psig=AOvVaw2phitVmoh9qTORSiCDaIw&ust=1695827096722000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCICora_GyIEDFQAAAAAdAAAAABAH', 'Jaunay-clan', '2023-09-20');
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Voiture Mazda', 'Mazda Rouge', 'Larry', 15000, 'ttps://www.ecosia.org/images?q=mazda%20rouge&addon=safari&addonversion=4.2.1.51#id=CAE8034B6B7B2EA738452C8456A8456172AD53EC ', 'Paris', '2023-09-19'); 
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Paris', 'Bleu', 'Emilie', 800, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=iphone%2013%20bleu#id=88ADE6B120D91711DC2480C92CCAC74846060C5B ', 'Bordeaux', '2023-09-20');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Ipad Pro', 'capacité 256GO', 'Victoria', 600, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=ipad%20pro#id=E511A995403262A4908DCE680DC837F16FE894C3', 'Paris', '2023-09-23');
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Spitz', 'Adulte de couleur noir', 'Emilie', 1200, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=spitz%20noir ', 'Lyon', '2023-09-21');
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Projecteur', 'de marque Samsung', 'Larry', 500, 'https://www.iacono.fr/fr/20681-samsung-sp-lsp3blaxxe.html ', 'Bordeaux', '2023-09-20');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Télé', '18 pouces', 'Larry', 0, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=chat ', 'Poitiers', '2023-09-24');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Ecouteurs Airpods', 'Neuve modèle pro', 'Larry', 400, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=airpod%20pro ', 'Bordeaux', '2023-09-20');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('maison F4', 'Maison de campagne F4', 'Larry', 400000, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=Maison%20F4 ', 'Guadeloupe', '2023-09-24');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Casque beats', 'Edition Mickey', 'Victoria', 300, 'https://www.ecosia.org/images?addon=safari&addonversion=4.2.1.51&q=casque%20beats%20mickey ', 'Japon', '2023-09-10');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Chapeau de paille', 'celui de luffy', 'Emilie', 200, 'https://www.ecosia.org/imagesq=chapeau%20de%20paille&addon=safari&addonversion=4.2.1.51#id=D4C88A6CAC7945D4403C86B688090A4F08FEF1BB ', 'Japon', '2023-09-24');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Mickey', 'Disneyland Paris', 'Emilie', 40, 'https://www.ecosia.org/images?q=peluche%20mickey%20collection&addon=safari&addonversion=4.2.1.51 ', 'Amérique', '2023-09-28');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Ordinateur', ' dans le magasin Boulanger', 'Boulanger', 200, 'https://www.fnac.com/PC-Portable-Gaming-Acer-Nitro-5-AN515-58-57GF-15-6-Intel-Core-i5-16-Go-RAM-512-Go-SSD-Noir/a17976933/w-4 ', 'Cergy', '2023-09-20');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES ('Stitch', 'Disney', 'Emilie', 40, ' https://www.shopdisney.fr/personnages-et-films/personnages/stitch?start=0&sz=24 ', 'Marne-la-vallée', '2023-09-24');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES('sac à dos', 'QuickSilver', 'Belle', 20, 'https://www.zalando.fr/quiksilver-secret-sesh-sac-a-dos-black-qu152n03g-q11.html ' ,  'Cergy', '2023-09-20');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES('Rulan', 'chat noir' , 'Laïna', 150, ' https://www.30millionsdamis.fr/actualites/article/19684-journee-mondiale-du-chat-noir-rendons-leur-leur-noblesse/ ', 'Orgeval', '2023-09-29');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES('Lampe',  'pied bois, et coupole blanche', 'Laïna', 15, 'https://www.leroymerlin.fr/produits/decoration-eclairage/luminaire-interieur/lampe/lampe-sur-pied-lampe-de-salon/lampe-molu-h-34-5-cm-blanc-atmosphera-83650818.html ', 'Osny', '2023-09-25' );
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES('Plaid 200 cm', 'Plaid fait main', 'Eloïse', 50, ' https://www.ksl-living.fr/plaids-laine/plaid-en-laine-merinos-grosses-mailles-tricot-fait-main-6-tailles-20-couleurs-basic-par-panapufa-6415.html ', 'Lyon', '2023-09-29');
+
+
+INSERT INTO ad (title, description, owner, price, picture, location, createdAt)
+VALUES('Douze tasses', 'collection dorée', 'Aryan', 1000, 'https://www.pinterest.fr/pin/112027109476414447/ ', 'Paris', '2023-09-20');
+
+
+SELECT *
+FROM ad 
+WHERE location = 'bordeaux';
